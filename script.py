c4a10062abb1bc3ec9c602e310b2d987da071e74
Sergey <physiho@yahoo.com>
2023-02-14 16:24:01 +0400

    update

--- script.py
+++ script.py
@@ -1,18 +1,21 @@
 from config import api
+import functools
 
 
+@functools.cache
 def get_coordinates(cadastral):
     from rosreestr2coord import Area
     import webbrowser
+
     try:
-        location = Area(cadastral).get_coord()
-        return list_to_dict(location)
+        location = Area(cadastral, with_proxy=True).get_coord()
     except:
         # if connection to rosreestr was broken:
-        location = Area(cadastral).get_coord()
+        location = Area(cadastral, with_proxy=True).get_coord()
         url = f'https://pkk.rosreestr.ru/api/features/1/{cadastral}'
         webbrowser.open(url)
-        return list_to_dict(location)
+
+    return list_to_dict(location)
 
 
 def list_to_dict(lst):
@@ -33,29 +36,47 @@ def list_to_dict(lst):
 
 def create_area(cadastral):
     from rosreestr2coord import Area
-    with api.Changeset({u"comment": u"Test area from Rosreestr"}):
-        coordinates = get_coordinates(cadastral)
+    import json
 
-        # creates list of nodes for an area:
-        ids = [api.NodeCreate({u"lon": k, u"lat": v, u"tag": {}})['id'] for k, v in coordinates.items()]
+    try:
+        with open('cache.json', 'r') as fp:
+            cache = json.load(fp)
+    except (FileNotFoundError, json.JSONDecodeError):
+        cache = {}
 
-        # adds the first node at the end of list of nodes to get a closed area:
-        ids.append(ids[0])
+    if cadastral not in cache:
+        with api.Changeset({u"comment": u"Test area from Rosreestr"}):
+            coordinates = get_coordinates(cadastral)
 
-        # creates an area from list of nodes:
-        ways = api.WayCreate({
-            'nd': ids,
-            'tag': {
-                'landuse': 'construction',
-                'назначение': Area(cadastral).get_attrs()['util_by_doc'],
-                'cadastral_number': cadastral,
-                'name': 'Test_Cad',
-                'is_in:country': 'Russia',
-            }})
+            # creates list of nodes for an area:
+            ids = [api.NodeCreate({u"lon": k, u"lat": v, u"tag": {}})['id'] for k, v in coordinates.items()]
 
-        return ways['id']
+            # adds the first node at the end of list of nodes to get a closed area:
+            ids.append(ids[0])
+
+            # creates an area from list of nodes:
+            ways = api.WayCreate({
+                'nd': ids,
+                'tag': {
+                    'name': cadastral,
+                    'is_in:country': 'Russia',
+                    'utility': Area(cadastral).get_attrs()['util_by_doc'],
+                    'address': Area(cadastral).get_attrs()['address'],
+                    'landuse': 'construction',
+                }})
+
+            cache[cadastral] = ways
+
+            with open('cache.json', 'w') as fp:
+                json.dump(cache, fp)
+
+    else:
+        ways = cache[cadastral]
+
+    return ways['id']
 
 
+@functools.cache
 def get_X_Y(cadastral):
     id = create_area(cadastral)
     way = api.WayFull(id)[0]['data']
@@ -68,3 +89,4 @@ def get_X(cadastral):
 
 def get_Y(cadastral):
     return get_X_Y(cadastral)[1]
+
